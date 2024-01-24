15e1703e9dad85775c853d87e5d1a1b3b3b5f918
ArchieWatsonIrvine <157597766+ArchieWatsonIrvine@users.noreply.github.com>
2024-01-24 12:39:02 +0000

    Controls

---
+++ Controls/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Travel_App.Helper;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static Android.Icu.Text.CaseMap;
+using static Android.Util.EventLogTags;
+
+namespace Travel_App
+{
+    public partial class App : Application
+    {
+        internal static IEnumerable<object> tp;
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Documents();
+        }
+
+        protected override void OnStart()
+        {
+
+            //generate list
+            Trip tp = new Trip("", "", 0)
+            {
+                Title = "Brighton",
+                Description = "Brighton testing description",
+                ID = 1
+            };
+            TripHelper.addTrip(tp);
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
